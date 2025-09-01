fa98906dc3c1214dd3c3de85f7ca75f72844f90a
idevemohan <sharewithdeve@gmail.com>
2025-09-01 14:28:12 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -15,7 +15,7 @@ app.use(cors({
   methods: ['GET','POST','PUT','DELETE','OPTIONS'],
   allowedHeaders: ['Content-Type','Authorization'],
   credentials: true
-})))
+}))
 
 
 app.options('*', cors());
